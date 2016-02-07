464aaa53f798875ceaf67f00c2125842642b03cd
hiteshbargujar <hitesh.bargujar@hobsons.com>
2016-02-07 07:00:42 -0500

    Problem statement for uncheckedException example

---
+++ src/main/java/org/javainterviewprep/examples/uncheckedException/UncheckedExceptionExample.java
@@ -0,0 +1,19 @@
+package org.javainterviewprep.examples.uncheckedException;
+
+/*
+ * Q1. What is Runtime Exception or UnChecked Exception ?
+ * - The exceptions which occur during execution of the Program are called Runtime Exceptions.
+ * - These exceptions are usually caused by any logical errors in the Program or during execution which depend on other systems which at time may behave faulty.
+ * - Some commonly encountered Runtime exceptions are:
+ - NullPointerException
+ - ArrayIndexOutOfBoundException
+ - ClassNotFoundException
+ */
+
+//Create an ArrayList which store five names.
+//Traverse through the names and print them.
+//Traverse using index to the size of the Names ArrayList
+//If encountered by ArrayIndexOutOfBoundException handle it gracefully or correct the code.
+public class UncheckedExceptionExample {
+
+}
